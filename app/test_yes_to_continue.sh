345b0846f282aaf77f0b1c14916086d613e5f832
David Rabkin <david@rabkin.co.il>
2022-01-19 00:21:00 +0200

    Add arguments parsing, no verbose by default

---
+++ app/test_yes_to_continue.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -eu
+# vi:ts=2 sw=2 tw=79 et lbr wrap
+# Copyright 2022 by David Rabkin
+
+# shellcheck source=../inc/base
+. "$(dirname "$(realpath "$0")")/../inc/base"
+
+yes_to_continue
+echo 'Continued.'
