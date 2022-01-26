5746f08e53ee665e1e4680f9ebe3050e0981ae7a
David Rabkin <david@rabkin.co.il>
2022-01-26 18:43:30 +0200

    Add echo function to handle echo on diffrent shells

---
+++ app/test_echo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -eu
+# vi:ts=2 sw=2 tw=79 et lbr wrap
+# Copyright 2022 by David Rabkin
+
+# shellcheck disable=SC1091
+. "$(dirname "$(realpath "$0")")/../inc/echo"
+
+echo hello
+# shellcheck disable=SC3037
+echo "-e"
+# shellcheck disable=SC2028
+echo "\\n"
+# shellcheck disable=SC3037
+echo -n hello
+# shellcheck disable=SC3037
+echo -ne hello
+echo bye
