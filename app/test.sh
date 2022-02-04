32635364fa9e9545064e26fefd91d3484a7373c4
David Rabkin <david@rabkin.co.il>
2022-02-04 23:24:44 +0200

    Change copyright notice form

--- app/test.sh
+++ app/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 # vi:ts=2 sw=2 tw=79 et lbr wrap
-# Copyright 2021 by David Rabkin
+# Copyright 2021-2022 David Rabkin
 
 # shellcheck source=../inc/base
 . "$(dirname "$(realpath "$0")")/../inc/base"
