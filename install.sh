02076bbda9fa6ec35a32ba407557b1eaab1e7cde
David Rabkin <david@rabkin.co.il>
2022-06-13 19:08:12 +0300

    Use parameters as a question in yes_to_continue

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # vi:et lbr noet sw=2 ts=2 tw=79 wrap
 # Copyright 2022 David Rabkin
 set -- --quiet "$@"
-BASE="$(dirname "$(realpath "$0")")/inc/base"
+BASE="$(dirname "$(realpath "$0")")"/inc/base
 
 # shellcheck source=./inc/base
 . "$BASE"
@@ -11,13 +11,12 @@ TGT=$DST/shellbase
 file_exists $DST || die $DST: No such directory.
 is_writable $DST || die $DST is not writable.
 if file_exists $TGT; then
-	printf \
-		'%s is already installed. Install %s?\n' \
+	yes_to_continue \
 		"$(sh $TGT --quiet --version)" \
-		"$BASE_VERSION"
-	yes_to_continue
+		is already installed. Install \
+		"$BASE_VERSION"?
 fi
-cp -f "$BASE" $TGT || die "Unable to copy $BASE to $TGT."
+cp -f "$BASE" $TGT || die Unable to copy "$BASE" to $TGT.
 printf \
 	'%s is installed to %s.\n' \
 	"$(sh $TGT --quiet --version)" \
