c82a13112db5d7640c1ba7ff4c9dd286f332266c
David Rabkin <david@rabkin.co.il>
2022-07-15 21:37:04 +0300

    Add prettytable

---
+++ app/test_prettytable_okey.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -eu
+# vi:et lbr noet sw=2 ts=2 tw=79 wrap
+# Copyright 2021-2022 David Rabkin
+
+# shellcheck source=../inc/base
+. "$(dirname "$(realpath "$0")")/../inc/base"
+
+carbon=$(cat <<- EOM
+	+-----------+----------+----------+
+	|ID         |NAME      |TITLE     |
+	+-----------+----------+----------+
+	|123456789  |John Foo  |Director  |
+	|12         |Mike Bar  |Engineer  |
+	+-----------+----------+----------+
+EOM
+)
+
+out="$({
+	printf 'ID\tNAME\tTITLE\n'
+	printf '123456789\tJohn Foo\tDirector\n'
+	printf '12\tMike Bar\tEngineer\n'
+} | \
+	prettytable \
+	sed -n '2,7p' | \
+	awk '{ print substr($0, index($0,$3)) }')"
+
+[ "$carbon" = "$out" ]
