49639000933a0a9d56af04141c00d4d5d58a6b56
David Rabkin <david@rabkin.co.il>
2022-08-11 06:24:35 +0300

    Add colors to logger, add timestamp to API

---
+++ app/test_log_okey.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -eu
+# vi:et lbr noet sw=2 ts=2 tw=79 wrap
+# Copyright 2022 David Rabkin
+
+# shellcheck source=../inc/base
+. "$(dirname "$(realpath "$0")")/../inc/base"
+
+i=0
+while [ $i -ne 10 ]; do
+	case "$((i%3))" in
+		0) log "$i" ;;
+		1) logw "$i" ;;
+		2) loge "$i" ;;
+	esac
+	i="$((i+1))"
+done
+exit 0
