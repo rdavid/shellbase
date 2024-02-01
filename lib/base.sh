a2422e8b825f5632ad359060dcaf125566f05b44
David Rabkin <david@rabkin.co.il>
2024-02-02 00:49:01 +0200

    New release v0.9.20240202

--- lib/base.sh
+++ lib/base.sh
@@ -47,7 +47,7 @@ BASE_DIR_WIP=/tmp
 BASE_FORK_CNT=0
 BASE_KEEP_WIP=false
 BASE_QUIET=false
-BASE_VERSION=0.9.20240128
+BASE_VERSION=0.9.20240202
 BASE_YES_TO_CONT=false
 
 # Removes any file besides mp3, m4a, flac in the current directory.
